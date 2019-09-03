cde3d3053bcd56af881e3a390b435103543ad998
Tom Brady the fucking GOAT <tim.buchholz98@gmx.de>
2019-09-03 10:47:15 +0200

    falsche namen

--- Aufgabe32.py
+++ Aufgabe32.py
@@ -6,17 +6,17 @@ from mppstart import *
 
 
 def main_32():
-    diffls = [0.000001]
+    diffls = [0.00001,0.000001]
     for diff in diffls:
         delete_old()
         manipulate_conf("m++conf", [("loadconf", "hybridreaction.conf")])
         manipulate_conf("hybridreaction.conf",
                         [("HybridProblem", "HybridReaction"), ("Discretization", "linear"), ("level", "2"),
                          ("T", "1.6"),
-                         ("dt", "0.025"), ("Diffusion", str(diff)), ("delta", "0.1"), ("Reaction", "0"),
+                         ("dt", "0.025"), ("Diffusion", str(diff)), ("delta", "1"), ("Reaction", "5"),
                          ("Model", "HybridReaction"), ("Convection", "1")])
         output = run()
-        name = "delta=0.1_diffusion=" + str(diff) + "Reaktion=0"
+        name = "delta=1_diffusion=" + str(diff) + "Reaktion=0"
         print(name)
         print("done")
         save("Aufgabe32", name)
