9cef682e237d25e994f5320fa788f9b12083573a
Tom Brady the fucking GOAT <tim.buchholz98@gmx.de>
2019-09-04 11:18:12 +0200

    MMMM

--- aufgabe001.py
+++ aufgabe001.py
@@ -15,5 +15,25 @@ def parseplot():
     plt.grid(True)
     plt.savefig("Aufgabe24/Level2Deg2/plotmasse.png")
 
+import numpy as np
+import matplotlib.pyplot as plt
+from mppstart import *
+
+def main():
+    outp = []
+    for lvl in ["0","1","2","3"]:
+        delete_old()
+        manipulate_conf("m++conf", [("loadconf","laplace.conf")])
+        manipulate_conf("laplace.conf",
+                        [("level",lvl), ("Mesh","UnitSquare"),("Discretization","linear")])
+        output = run()
+        out = parse_mpp_output_single_inform("Problem size",output)
+        outp.append(out)
+        name = "Test2_"+lvl
+        save(None, name)
+    print(outp)
+
+
 if __name__ == "__main__":
-    parseplot()
+    main()
+
