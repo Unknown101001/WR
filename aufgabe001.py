7d2839fdd00b81a950d6dfcceeed484417dacfc3
Tom Brady the fucking GOAT <tim.buchholz98@gmx.de>
2019-09-07 15:26:09 +0200

    abb5

--- aufgabe001.py
+++ aufgabe001.py
@@ -21,20 +21,47 @@ from mppstart import *
 
 def main():
     outp = []
-    for lvl in ["8"]:
-        delete_old()
-        manipulate_conf("m++conf", [("loadconf","laplace.conf")])
-        manipulate_conf("laplace.conf",
-                        [("level",lvl),("plevel","7"),("Preconditioner","GaussSeidel"),
-                         ("Mesh","UnitSquare"),("Discretization","linear"),("Problem", "Discontinuous"),("LinearSteps","1000")])
-        output = run()
-        out = parse_mpp_output_single_inform("Problem size",output)
-        outp.append(out)
-        name = "Test3_"+lvl
-        save(None, name)
-    print(outp)
+    for prob in ["Discontinuous","Simple"]:
+        for lvl in ["1","2","3","4","5","6","7","8"]:
+            delete_old()
+            manipulate_conf("m++conf", [("loadconf","laplace.conf")])
+            manipulate_conf("laplace.conf",
+                            [("level",lvl),("plevel","7"),("Preconditioner","Multigrid"),
+                             ("Mesh","UnitSquare"),("Discretization","linear"),("Problem", prob),("LinearSteps","1000")])
+            output = run()
+            name = "test_"+lvl+"_"+prob
+            save("Abb5", name)
+
+def write_to_csv(saveload):
+    outputfile = "Abb5/data.csv"
+    with open(outputfile, 'w', newline='', encoding='utf-8')as csvfile:
+        writer = csv.writer(csvfile, delimiter=';')
+        writer.writerow([a[0] for a in saveload])  # names
+        writer.writerow([int(a[1]) for a in saveload])  # Problem Size
+        writer.writerow([float(a[2]) for a in saveload])  # Inflow
+        writer.writerow([float(a[3]) for a in saveload])  # Outflow
+        writer.writerow([float(a[4]) for a in saveload])  # Flux Error
+        writer.writerow([float(a[5]) for a in saveload])  # Flux Loss
+        writer.writerow([a[6] for a in saveload])  # time
+        writer.writerow([a[7] for a in saveload])  # einheit
+
+def read_values():
+    saveload = []
+    for prob in ["Discontinuous", "Simple"]:
+        for lvl in ["1","2","3","4","5","6","7","8"]:
+            name = "test_"+lvl+"_"+prob
+            logfile = "Abb5/" + name + "/log"
+            out = parse_mpp_output_single_inform(["Problem size", "Inflow", "Outflow", "Flux Error", "Flux Loss"],
+                                                 logfile=logfile)
+            time, einheit = get_time(logfile=logfile)
+            liste = [name, out[0][0], out[1][0], out[2][0], out[3][0], out[4][0], time, einheit]
+            # print(liste)
+            saveload.append(liste)
+    return saveload
 
 
 if __name__ == "__main__":
     main()
+    saveload = read_values()
+    write_to_csv(saveload)
 
